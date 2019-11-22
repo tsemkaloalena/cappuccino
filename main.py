466771592aee0c81328d1ceec84c7fe41e82ff3d
tsemkaloalena <alenatsemkalo@gmail.com>
2019-11-22 15:15:59 +0300

    First commit

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ class MyWidget(QWidget):
         super().__init__()
         uic.loadUi('main.ui', self)
         self.loadUi('coffee.db')
+        self.btn.clicked.connect(self.change_table)
 
     def loadUi(self, dbname):
         con = sqlite3.connect(dbname)
@@ -28,8 +29,55 @@ class MyWidget(QWidget):
 
         self.tableWidget.resizeColumnsToContents()
 
+    def change_table(self):
+        self.change_form = addEditCoffeeForm(self, self.db)
+        self.change_form.show()
 
-app = QApplication(sys.argv)
-ex = MyWidget()
-ex.show()
-sys.exit(app.exec_())
+
+class addEditCoffeeForm(QWidget):
+    def __init__(self, *db):
+        super().__init__()
+        uic.loadUi('addEditCoffeeForm.ui', self)
+        self.db = db[0]
+        self.loadUi()
+        self.tableWidget.itemChanged.connect(self.item_changed)
+        self.save_btn.clicked.connect(self.save_table)
+        self.modified = {}
+
+    def loadUi(self):
+        con = sqlite3.connect(self.db)
+        cur = con.cursor()
+        result = cur.execute("SELECT * FROM about").fetchall()
+        title = ['ID', 'Название сорта', 'Степень обжарки', 'Молотый/в зернах', 'Описание вкуса', 'Цена',
+                 'Объём упаковки']
+
+        self.tableWidget.setColumnCount(len(title))
+        self.tableWidget.setHorizontalHeaderLabels(title)
+
+        self.tableWidget.setRowCount(0)
+        for i, row in enumerate(result):
+            self.tableWidget.setRowCount(self.tableWidget.rowCount() + 1)
+            for j, elem in enumerate(row):
+                self.tableWidget.setItem(i, j, QTableWidgetItem(str(elem)))
+        self.tableWidget.resizeColumnsToContents()
+        self.modified = {}
+
+    def item_changed(self, item):
+        self.modified[self.titles[item.column()]] = item.text()
+
+    def save_results(self):
+        if self.modified:
+            cur = self.con.cursor()
+            que = "UPDATE about SET\n"
+            for key in self.modified.keys():
+                que += "{}='{}'\n".format(key, self.modified.get(key))
+            que += "WHERE id = ?"
+            cur.execute(que, (self.spinBox.text(),))
+            self.con.commit()
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MyWidget()
+    ex.show()
+    sys.exit(app.exec())
